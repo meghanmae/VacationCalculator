904df990ebc6305517abf25376691e7bcfb6d450
Meg <mmwoodfo@asu.edu>
2021-04-18 14:25:26 -0700

    Adding code

---
+++ Helpers.cs
@@ -0,0 +1,61 @@
+﻿using NodaTime;
+using System;
+using System.Globalization;
+
+namespace VacationCalculator
+{
+    public enum DateType
+    {
+        START,
+        END
+    }
+
+    class Helpers
+    {
+        private static double MonthlyVacation { get; } = 6.667; // 6 hours and 40 minutes accumulated per month
+        private static double ExtraVacationPerYear { get; } = 0.667; // Extra 40 minutes accumulated per year there
+
+        public static double GetVacationHours(DateTime startDate, DateTime endDate, double daysOff = 0)
+        {
+            // Get number of months total
+            if (startDate > endDate) return 0;
+            int months = MonthDiff(startDate, endDate);
+
+            // Get number of years after start date for extra time
+            LocalDate start = new(startDate.Year, startDate.Month, startDate.Day);
+            LocalDate end = new(endDate.Year, endDate.Month, endDate.Day);
+            int years = Period.Between(start, end).Years;
+
+            return (months * ((years * ExtraVacationPerYear) + MonthlyVacation)) - (daysOff * 8);
+        }
+
+        public static double GetFutureVacationHorusFromCurrentVacationTime(DateTime startDate, DateTime endDate, double currentVacation, double daysOff = 0)
+        {
+            // Get number of months total
+            if (startDate > endDate) return 0;
+            int months = MonthDiff(DateTime.Now, endDate);
+
+            // Get number of years after start date for extra time
+            LocalDate start = new(startDate.Year, startDate.Month, startDate.Day);
+            LocalDate end = new(endDate.Year, endDate.Month, endDate.Day);
+            int years = Period.Between(start, end).Years;
+
+            double futureTime = (months * ((years * ExtraVacationPerYear) + MonthlyVacation)) + currentVacation;
+            return futureTime - (daysOff * 8);
+        }
+
+        public static int MonthDiff(DateTime d1, DateTime d2)
+        {
+            int m1;
+            int m2;
+
+            if (d1.Month == d2.Month && d1.Year == d2.Year) return 0;
+
+            m1 = (d2.Month - d1.Month);//for years
+            m2 = (d2.Year - d1.Year) * 12; //for months
+
+
+            return m1 + m2 - 1;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VacationCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static DateTime DefaultStartDate { get; } = new DateTime(2020, 6, 17, 0, 0, 0);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            futureDatePicker.SelectedDate = DateTime.Now;
+            ToggleFutureVacationInformationVisibility();
+            startDatePicker.SelectedDate = DefaultStartDate;
+            totalHoursValueLabel.Content = Helpers.GetVacationHours(DefaultStartDate, DateTime.Now, 0);
+
+            ToggleErrorMessage();
+        }
+
+        private void StartDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            totalHoursValueLabel.Content = Helpers.GetVacationHours(startDatePicker!.SelectedDate!.Value, DateTime.Now, 0);
+        }
+
+        private void CurrentVacationHoursEntered(object sender, TextChangedEventArgs e)
+        {
+            if(double.TryParse(currentHoursTextBox.Text, out _))
+            {
+                ToggleErrorMessage(alwaysHidden: true);
+                ToggleFutureVacationInformationVisibility(alwaysVisible: true);
+            }
+            else
+            {
+                ToggleFutureVacationInformationVisibility(alwaysHidden: true);
+                ToggleErrorMessage(alwaysVisible: true, message: $"'{currentHoursTextBox.Text}' is not a valid number.");
+            }
+        }
+
+        private void FutureDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GetFutureVacationTime();
+        }
+
+        private void PlannedTimeOffEntered(object sender, TextChangedEventArgs e)
+        {
+            GetFutureVacationTime();
+        }
+
+        private void ToggleErrorMessage(bool alwaysVisible = false, bool alwaysHidden = false, string message = "Something went wrong!")
+        {
+            // Hide
+            if(alwaysHidden || errorLabel.Visibility == Visibility.Visible && !alwaysVisible)
+            {
+                errorLabel.Content = "";
+                errorLabel.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                errorLabel.Content = message;
+                errorLabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void GetFutureVacationTime()
+        {
+            if (startDatePicker is not null && futureDatePicker is not null)
+                if (startDatePicker.SelectedDate is not null && futureDatePicker.SelectedDate is not null)
+                {
+                    if (double.TryParse(currentHoursTextBox.Text, out double currentHours))
+                    {
+                        if (double.TryParse(plannedTimeOffTextBox.Text, out double plannedTimeOff))
+                        {
+                            ToggleErrorMessage(alwaysHidden: true);
+                            totalVacationLeftValueLabel.Content = Helpers.GetFutureVacationHorusFromCurrentVacationTime(
+                                startDatePicker.SelectedDate.Value,
+                                futureDatePicker.SelectedDate.Value,
+                                currentHours,
+                                plannedTimeOff);
+                        }
+                        else
+                        {
+                            ToggleErrorMessage(alwaysVisible: true, message: $"'{plannedTimeOffTextBox.Text}' is not a valid number.");
+                        }
+                    }
+                    else
+                    {
+                        ToggleErrorMessage(alwaysVisible: true, message: $"'{currentHoursTextBox.Text}' is not a valid number.");
+                    }
+                }
+        }
+
+        private void ToggleFutureVacationInformationVisibility(bool alwaysVisible = false, bool alwaysHidden = false)
+        {
+            // Hide
+            if (alwaysHidden || futureDateLabel.Visibility == Visibility.Visible && !alwaysVisible)
+            {
+                futureDateLabel.Visibility = Visibility.Hidden;
+                futureDatePicker.Visibility = Visibility.Hidden;
+
+                totalVacationLeftlabel.Visibility = Visibility.Hidden;
+                totalVacationLeftValueLabel.Visibility = Visibility.Hidden;
+
+                plannedTimeOffLabel.Visibility = Visibility.Hidden;
+                plannedTimeOffTextBox.Visibility = Visibility.Hidden;
+            }
+            // Show
+            else
+            {
+                futureDateLabel.Visibility = Visibility.Visible;
+                futureDatePicker.Visibility = Visibility.Visible;
+
+                totalVacationLeftlabel.Visibility = Visibility.Visible;
+                totalVacationLeftValueLabel.Visibility = Visibility.Visible;
+
+                plannedTimeOffLabel.Visibility = Visibility.Visible;
+                plannedTimeOffTextBox.Visibility = Visibility.Visible;
+
+                // Set values
+                futureDatePicker.SelectedDate = DateTime.Now;
+                totalVacationLeftValueLabel.Content = currentHoursTextBox.Text;
+                plannedTimeOffTextBox.Text = "0";
+            }
+        }
+
+    }
+}
